f525f2c369b62f409c7b10e2c750615a211d525f
TimHubert <Tim.Hubert@Student.HTW-Berlin.de>
2025-04-30 20:16:55 +0200

    Change URL

--- frontend/src/components/WorkoutView.vue
+++ frontend/src/components/WorkoutView.vue
@@ -69,7 +69,7 @@ const workout = ref({
 
 onMounted(() => {
   const apiUrl = import.meta.env.VITE_APP_BACKEND_BASE_URL
-  fetch(`${apiUrl}/api/workout`)
+  fetch(`${apiUrl}/workout`)
     .then((response) => response.json())
     .then((data) => {
       const rawDate = new Date(data.date)
