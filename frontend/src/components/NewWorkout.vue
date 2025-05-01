1827b886c4c75332babe37dc166bb43d90cdabb6
TimHubert <Tim.Hubert@Student.HTW-Berlin.de>
2025-05-01 14:19:34 +0200

    Add cdd to newWorkout

--- frontend/src/components/NewWorkout.vue
+++ frontend/src/components/NewWorkout.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <h1>Neues Workout</h1>
-    <input placeholder="Name des Workouts" required />
+    <input class="input-line" placeholder="Name des Workouts" required />
 
     <div v-if="zeigeÜbungen">
       <div v-for="(übung, index) in übungen" :key="index" class="exercise">
         <input v-model="übung.name" placeholder="Übung" required />
         <input v-model="übung.gerät" placeholder="Gerät" required />
-        <input v-model="übung.muskelgruppe" placeholder="Muskelgruppe" required />
+        <select v-model="übung.muskelgruppe" id="muskelgruppe">
+          <option disabled value="">Muskelgruppe</option>
+          <option v-for="gruppe in übung.muskelgruppen" :key="gruppe" :value="gruppe">
+            {{ gruppe }}
+          </option>
+        </select>
         <button @click="removeÜbung(index)" type="button">-</button>
       </div>
     </div>
 
-    <button @click="toggleÜbungen" type="button">+</button>
-    <button @click="submitWorkout" type="submit">Erstellen</button>
+    <button @click="toggleÜbungen" type="button" class="add">+</button>
+    <button @click="submitWorkout" type="submit" class="submit">Erstellen</button>
   </div>
 </template>
 
@@ -28,6 +33,7 @@ export default {
           name: '',
           gerät: '',
           muskelgruppe: '',
+          muskelgruppen: ['Brust', 'Lat', 'Trizeps', 'Bizeps', 'Schulter', 'Beine'],
         },
       ],
     }
@@ -42,7 +48,12 @@ export default {
       }
     },
     addÜbung() {
-      this.übungen.push({ name: '', gerät: '', muskelgruppe: '' })
+      this.übungen.push({
+        name: '',
+        gerät: '',
+        muskelgruppe: '',
+        muskelgruppen: ['Brust', 'Lat', 'Trizeps', 'Bizeps', 'Schulter', 'Beine'],
+      })
     },
     removeÜbung(index) {
       this.übungen.splice(index, 1)
@@ -105,6 +116,78 @@ onMounted(() => {
 </script>
 
 <style>
+.exercise {
+  margin-top: 0.3rem;
+}
+.exercise input,
+.exercise select {
+  margin-right: 0.5rem;
+  margin-top: 0.2rem;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 0px solid #ff7b00;
+  background-color: #474747;
+  color: white;
+  outline: none;
+}
+.exercise button {
+  margin-top: 0.2rem;
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  outline: none;
+}
+.exercise button:hover {
+  background-color: #ff1a1a;
+}
+
+.add {
+  background-color: #4caf50;
+  margin-top: 0.4rem;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  outline: none;
+}
+.add:hover {
+  background-color: #45a049;
+}
+
+.submit {
+  background-color: #ed6b00;
+  margin-top: 0.4rem;
+  margin-left: 0.5rem;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  outline: none;
+}
+.submit:hover {
+  background-color: #da720b;
+}
+
+.input-line {
+  padding: 0.5rem 0;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  background-color: transparent;
+  color: white;
+  outline: none;
+  width: 100%;
+  font-size: large;
+}
+
+.input-line:focus {
+  border-bottom: 2px solid #ff7b00;
+}
+
 .table {
   width: 100%;
   border-collapse: none;
